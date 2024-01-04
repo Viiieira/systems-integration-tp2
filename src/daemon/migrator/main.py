1f1e2d039f55dbf87623474b96194e62d7b3a29b
Hugo Vieira <hugo.vieira2002@hotmail.com>
2024-01-04 16:30:00 +0000

    minimal changes todo

--- src/daemon/migrator/main.py
+++ src/daemon/migrator/main.py
@@ -31,32 +31,6 @@ def print_psycopg2_exception(ex):
     print("pgerror:", ex.pgerror)
     print("pgcode:", ex.pgcode, "\n")
 
-def check_for_changes_in_imported_documents(connection):
-    try:
-        cursor = connection.cursor()
-
-        # TODO: Execute a SELECT query to check for any changes on the table
-        select_query = "SELECT * FROM imported_documents WHERE migrated = false"
-        cursor.execute(select_query)
-
-        # Fetch the results
-        results = cursor.fetchall()
-
-        # Process the results (you may want to return or further process these results)
-        for row in results:
-            print(f"Found new document: {row}")
-
-        # Commit the transaction
-        connection.commit()
-
-    except OperationalError as err:
-        print_psycopg2_exception(err)
-        connection.rollback()
-
-    finally:
-        if cursor:
-            cursor.close()
-
 if __name__ == "__main__":
 
     db_org = psycopg2.connect(host='db-xml', database='is', user='is', password='is')
@@ -79,6 +53,7 @@ if __name__ == "__main__":
 
         print("Checking updates...")
         # TODO: 1- Execute a SELECT query to check for any changes on the table
+        # TODO Isto vai ter de ir do migrator para o watcher
         try:
             changes_check(db_org)
         except Exception as e:
