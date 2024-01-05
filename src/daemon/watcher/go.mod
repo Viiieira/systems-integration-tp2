d2540b07e0e48ecbdeab18e767275da4b4c27cd4
Vitor Mina <148635045+vitorhmina@users.noreply.github.com>
2024-01-05 09:57:52 +0000

    Broker Connection refused

--- src/daemon/watcher/go.mod
+++ src/daemon/watcher/go.mod
@@ -3,3 +3,5 @@ module migrator
 go 1.21.5
 
 require github.com/lib/pq v1.10.9
+
+require github.com/streadway/amqp v1.1.0 // indirect
--- src/daemon/watcher/main.go
+++ src/daemon/watcher/main.go
@@ -1,29 +1,119 @@
 package main
 
 import (
-	"fmt"
 	"database/sql"
-	_ "github.com/lib/pq"
+	"encoding/xml"
+	"fmt"
 	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/streadway/amqp"
 )
 
+type Wine struct {
+	ID           int    `xml:"id,attr"`
+	Name         string `xml:"name,attr"`
+	Points       int    `xml:"points,attr"`
+	Price        string `xml:"price,attr"`
+	Variety      string `xml:"variety,attr"`
+	ProvinceRef  int    `xml:"province_ref,attr"`
+	TasterRef    int    `xml:"taster_ref,attr"`
+	WineryRef    int    `xml:"winery_ref,attr"`
+	Latitude     string `xml:"latitude,attr"`
+	Longitude    string `xml:"longitude,attr"`
+}
+
+type Province struct {
+	ID      int    `xml:"id,attr"`
+	Name    string `xml:"name,attr"`
+	CountryRef int `xml:"country_ref,attr"`
+	Latitude  string `xml:"latitude,attr"`
+	Longitude string `xml:"longitude,attr"`
+	Wines    []Wine `xml:"Wines>Wine"`
+}
+
+type Country struct {
+	ID       int       `xml:"id,attr"`
+	Name     string    `xml:"name,attr"`
+	Provinces []Province `xml:"Provinces>Province"`
+}
+
+type WineReviews struct {
+	Countries []Country `xml:"Countries>Country"`
+}
+
+func sendMessageToBroker(countryName string) {
+	conn, err := amqp.Dial("amqp://$RABBITMQ_DEFAULT_USER:$RABBITMQ_DEFAULT_PASS@localhost:5672/$RABBITMQ_DEFAULT_VHOST")
+	CheckError(err)
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	CheckError(err)
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"import_queue", // Queue name
+		false,          // Durable
+		false,          // Delete when unused
+		false,          // Exclusive
+		false,          // No-wait
+		nil,            // Arguments
+	)
+	CheckError(err)
+
+	err = ch.Publish(
+		"",           // Exchange
+		q.Name,       // Routing key
+		false,        // Mandatory
+		false,        // Immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(fmt.Sprintf("Import country: %s", countryName)),
+		},
+	)
+	CheckError(err)
+}
 
 func checkUnmigratedFiles(db *sql.DB) {
 	fmt.Println("Checking for unmigrated files...")
-	rows, err := db.Query("SELECT file_name FROM public.imported_documents WHERE migrated = FALSE AND deleted = FALSE")
+	rows, err := db.Query("SELECT file_name, xml FROM public.imported_documents WHERE migrated = FALSE AND deleted = FALSE")
 	CheckError(err)
 	defer rows.Close()
 
 	fmt.Println("Unmigrated Files:")
 	for rows.Next() {
-		var fileName string
-		err := rows.Scan(&fileName)
+		var fileName, xmlData string
+		err := rows.Scan(&fileName, &xmlData)
 		CheckError(err)
-		fmt.Printf("\t> %s\n", fileName)
-	}
 
-	err = rows.Err()
-	CheckError(err)
+		fmt.Printf("\nFile Name: %s\n", fileName)
+
+		// Parse the XML data
+		var wineReviews WineReviews
+		err = xml.Unmarshal([]byte(xmlData), &wineReviews)
+		if err != nil {
+			fmt.Printf("Error unmarshalling XML for file %s: %v\n", fileName, err)
+			continue
+		}
+
+		// Process the parsed data
+		processWineReviews(wineReviews)
+	}
+}
+
+func processWineReviews(wineReviews WineReviews) {
+	for _, country := range wineReviews.Countries {
+		fmt.Printf("Country: %s\n", country.Name)
+		sendMessageToBroker(country.Name) // Send message to the broker for each country
+
+		for _, province := range country.Provinces {
+			fmt.Printf("\tProvince: %s\n", province.Name)
+			for _, wine := range province.Wines {
+				fmt.Printf("\t\tWine: %s\n", wine.Name)
+				// Add your logic here based on the wine entity
+			}
+		}
+	}
 }
 
 func CheckError(err error) {
@@ -44,6 +134,6 @@ func main() {
 	}
 
 	fmt.Println("\nSuccessfully connected to the database!")
-	
+
 	checkUnmigratedFiles(db)
 }
